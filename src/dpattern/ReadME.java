317daedca378b1826bae87a3725e292fdb5d79a0
Priyantha Samaraweera <kdpsamaraweera@gmail.com>
2019-11-15 17:34:52 +0530

    Delete ReadME.java

--- src/dpattern/ReadME.java
+++
@@ -1,9 +0,0 @@
-package dpattern;
-
-public class ReadME {
-    /**
-     *Factory is one of the creational pattern
-     *The creational logic encapsulated withing the factory and it's provides a method that returns a newly created object.
-     *
-     */
-}
